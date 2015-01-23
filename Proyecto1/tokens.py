22509e52eb45f2bce45a1f839ddb222eb93f8c0a
MonicaFiguera <monica.yuncosa@gmail.com>
2015-01-23 01:54:45 -0430

    creo que no funciona

--- Proyecto1/tokens.py
+++ Proyecto1/tokens.py
@@ -8,214 +8,175 @@ class LexicalAnalyzer:
     error = False
     output = ""
     readingString = False
-    readingComment = False
     errorOutput = ''
     beginningOfLine = -1
     reserved = {
-        'if'      : 'If',
-        'else'    : 'Else',
-        'elif'    : 'Elif',
-        'def'     : 'Def',
-        'class'   : 'Class',
-        'while'   : 'While',
-        'for'     : 'for',
-        'and'     : 'And',
-        'or'      : 'Or',
-        'program' : 'Program',
-        'in'      : 'In',
-        'using'   : 'Using',
-        'scan'    : 'Scan',
-        'print'   : 'Print',
-        'println' : 'Println',
-        'int'     : 'Int',
-        'bool'    : 'Bool',
-        'set'     : 'Set',
-        'break'   : 'Break',
-        'true'    : 'True',
-        'false'   : 'False',
-        'not'     : 'Not',
-        'do'      : 'Do',
-        'repeat'  : 'Repeat',
-        'return'  : 'Return',
-        'min'     : 'Min',
-        'max'     : 'Max'
+        'if'      : 'IF',
+        'else'    : 'ELSE',
+        'elif'    : 'ELIF',
+        'def'     : 'DEF',
+        'class'   : 'CLASS',
+        'while'   : 'WHILE',
+        'for'     : 'FOR',
+        'and'     : 'AND',
+        'or'      : 'OR',
+        'program' : 'PROGRAM',
+        'in'      : 'IN',
+        'using'   : 'USING',
+        'scan'    : 'SCAN',
+        'print'   : 'PRINT',
+        'println' : 'PRINTLN',
+        'int'     : 'INT',
+        'bool'    : 'BOOL',
+        'set'     : 'SET',
+        'break'   : 'BREAK',
+        'true'    : 'TRUE',
+        'false'   : 'FALSE',
+        'not'     : 'NOT',
+        'do'      : 'DO',
+        'repeat'  : 'REPEAT',
+        'return'  : 'RETURN',
+        'min'     : 'MIN',
+        'max'     : 'MAX'
         }
     
     tokens = [
-       'NewLine',
-       'Number',
-       'Plus',
-       'Minus',
-       'Times',
-       'Divide',
-       'Module',
-       'ID',
-       'String',
-       'Comment',
-       'Comma',
-       'Semicolon',
-       'OpenCurly',
-       'CloseCurly',
-       'OpenBracket',
-       'CloseBracket',
-       'Space',
-       'Equals',
-       'Dot',
-       'Tab',
-       'Colon',
-       'SimpleQuote',
-       'CloseQuote',
-       'Union',
-       'Difference',
-       'Intersection',
-       'MappingPlus',
-       'MappinMinus',
-       'MappingTimes',
-       'MappingDivide',
-       'MappingModule',
-       'SetMaxValue',
-       'SetMinValue',
-       'SetSize',
-       'LessThan',
-       'GreaterThan',
-       'LessEqualThan',
-       'GreaterEqualThan',
-       'Equivalence',
-       'Inequivalence',
-       'BelongsTo',
-       'ReturnValue',
-       'NewLineEsc',
-       'QuoteEsc',
-       'BackSlashEsc',
+       'NEWLINE', 'NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
+       'MODULE', 'IDENTIFIER', 'STRING', 'COMMENT', 'COMMA',
+       'SEMICOLON', 'LCURLY', 'RCURLY', 'LBRACKET', 'RBRACKET',
+       'SPACE', 'ASSIGN', 'DOT', 'TAB', 'COLON', 'SIMPLEQUOTE',
+       'RDOUBLEQUOTE', 'SETUNION', 'SETDIFF', 'SETINTERSECT',
+       'SETMAPPLUS', 'SETMAPMINUS', 'SETMAPTIMES', 'SETMAPDIVIDE',
+       'SETMAPMODULE', 'SETMAXVALUE', 'SETMINVALUE', 'SETSIZE',
+       'LESSTHAN', 'GREATERTHAN', 'LESSEQUALTHAN', 'GREATEREQUALTHAN',
+       'EQUALS', 'NOTEQUALTO', 'BELONGSTO', 'NEWLINEESC',
+       'QUOTEESC', 'BACKSLASHESC'
     ] + list(reserved.values())
     
-    t_ANY_Plus = r'\+'
-    t_ANY_Minus = r'\-'
-    t_ANY_Times = r'\*'
-    t_ANY_Divide = r'\/'
-    t_ANY_Module = r'\%' 
-    t_ANY_Comma = r'\,'
-    t_ANY_Semicolon = r'\;'
-    t_ANY_OpenCurly = r'\{'
-    t_ANY_CloseCurly = r'\}'
-    t_ANY_OpenBracket = r'\('
-    t_ANY_CloseBracket = r'\)'
-    t_ANY_Space = r'\ '
-    t_ANY_Tab = r'\t'
-    t_ANY_Equals = r'\='
-    t_ANY_Dot = r'\.'
-    t_ANY_Colon = r'\:'
-    t_ANY_SimpleQuote = r'\''
-    t_ANY_ReturnValue = r"\-\>"  # (?)
+    t_ANY_PLUS      = r'\+'
+    t_ANY_MINUS     = r'\-'
+    t_ANY_TIMES     = r'\*'
+    t_ANY_DIVIDE    = r'\/'
+    t_ANY_MODULE    = r'\%' 
+    t_ANY_COMMA     = r'\,'
+    t_ANY_SEMICOLON = r'\;'
+    t_ANY_LCURLY    = r'\{'
+    t_ANY_RCURLY    = r'\}'
+    t_ANY_LBRACKET  = r'\('
+    t_ANY_RBRACKET  = r'\)'
+    t_ANY_SPACE     = r'\ '
+    t_ANY_TAB       = r'\t'
+    t_ANY_ASSIGN    = r'\='
+    t_ANY_DOT       = r'\.'
+    t_ANY_COLON     = r'\:'
+    t_ANY_SIMPLEQUOTE = r'\''
     
     # Set operators
-    t_ANY_Union = r'\+\+'  # (?)
-    t_ANY_Difference = r'\\'  # (?)
-    t_ANY_Intersection = r'\>\<'
-    t_ANY_MappingPlus = r'\<\+\>'
-    t_ANY_MappinMinus = r'\<\-\>'
-    t_ANY_MappingTimes = r'\<\*\>'
-    t_ANY_MappingDivide = r'\<\/\>'
-    t_ANY_MappingModule = r'\<\%\>'
-    t_ANY_SetMaxValue = r'\>\?'
-    t_ANY_SetMinValue = r'\<\?'
-    t_ANY_SetSize = r'\$\?'
+    t_ANY_SETUNION      = r'\+\+'
+    t_ANY_SETDIFF       = r'\\'
+    t_ANY_SETINTERSECT  = r'\>\<'
+    t_ANY_SETMAPPLUS    = r'\<\+\>'
+    t_ANY_SETMAPMINUS   = r'\<\-\>'
+    t_ANY_SETMAPTIMES   = r'\<\*\>'
+    t_ANY_SETMAPDIVIDE  = r'\<\/\>'
+    t_ANY_SETMAPMODULE  = r'\<\%\>'
+    t_ANY_SETMAXVALUE   = r'\>\?'
+    t_ANY_SETMINVALUE   = r'\<\?'
+    t_ANY_SETSIZE       = r'\$\?'
 
     # Bool operators
-    t_ANY_LessThan = r'\<' 
-    t_ANY_GreaterThan = r'\>'
-    t_ANY_LessEqualThan = r'\<\='
-    t_ANY_GreaterEqualThan = r'\>\=' 
-    t_ANY_Equivalence = r'\=\='
-    t_ANY_Inequivalence = r'\/\=' 
-    t_ANY_BelongsTo = r'\@'
+    t_ANY_LESSTHAN      = r'\<' 
+    t_ANY_GREATERTHAN   = r'\>'
+    t_ANY_LESSEQUALTHAN = r'\<\='
+    t_ANY_GREATEREQUALTHAN = r'\>\=' 
+    t_ANY_EQUALS        = r'\=\='
+    t_ANY_NOTEQUALTO    = r'\/\=' 
+    t_ANY_BELONGSTO     = r'\@'
     
-    #Escape Sequence
-
     states = (
-        ('String','exclusive'),
+        ('STRING','exclusive'),
     )
 
-    def t_ANY_Number(self, t):
+    def t_ANY_NUMBER(self, t):
         r'\d+'
         t.value = int(t.value)    
         return t
 
-    def t_ANY_NewLine(self, t):
+    def t_ANY_IDENTIFIER(self, t):
+        r'[a-zA-Z_][a-zA-Z_0-9]*'
+        if (self.reserved.get(t.value, 'IDENTIFIER') != 'IDENTIFIER'):      # Es una palabra reservada
+            t.type = self.reserved.get(t.value, 'IDENTIFIER')
+        elif (self.reserved.get(t.value, 'IDENTIFIER') == 'IDENTIFIER'):    # Es un ID
+            t.type = self.reserved.get(t.value, 'IDENTIFIER')
+        return t
+
+    def t_ANY_NEWLINE(self, t):
         r'\n'
         self.beginningOfLine = t.lexpos
         t.lexer.lineno += len(t.value)
         return t 
 
-    def t_String(self,t):
+    def t_STRING(self,t):
         r'\"'
-        t.lexer.begin('String')
+        self.readingString = True
+        t.lexer.begin('STRING')
         return t
 
-    def t_String_CloseQuote(self,t):
+    def t_STRING_RDOUBLEQUOTE(self,t):
         r'\"'
+        self.readingString = False
         t.lexer.begin('INITIAL')
         return t
 
-    def t_String_NewLineEsc(self,t):
+    def t_STRING_NEWLINEESC(self,t):
         r'\\n'
         return t
         
-    def t_String_QuoteEsc(self,t):
-            r'\\\"'
-            return t
-    def t_String_BackSlashEsc(self,t):
-            r'\\\\'
-            return t
-    
-    def t_ANY_ID(self, t):
-        r'[a-zA-Z_][a-zA-Z_0-9]*'
-    
-        if (self.reserved.get(t.value, 'ID') != 'ID'):  # Es una palabra reservada
-            t.type = self.reserved.get(t.value, 'ID')
-        elif (self.reserved.get(t.value, 'ID') == 'ID'):  # Es un ID
-            t.type = self.reserved.get(t.value, 'ID')
+    def t_STRING_QUOTEESC(self,t):
+        r'\\\"'
         return t
-        
-       
-    
-    def t_ANY_error(self, t):
-        if self.readingString:
-          print t.value[0]
-          self.output += t.value[0]
-        elif not self.readingComment:
-          self.errorOutput += '''Error: Se encontró un caracter inesperado "%s" en la Línea %d, Columna %d\n''' % (t.value[0], t.lineno, t.lexpos - self.beginningOfLine)
-          self.error = True
-        t.lexer.skip(1)
-    
+
+    def t_STRING_BACKSLASHESC(self,t):
+        r'\\\\'
+        return t
+            
     # Funcion que atrapa todo el string dentro de comillas.
-
-
-    def String(self, t):
+    def GetString(self, t):
         quoteType = t.type
         quote = t.value
-        self.output += "TokenString: %s" % t.value
+        self.output += "token STRING\t\tvalue (%s" % t.value
         n = t.lexpos - self.beginningOfLine
         m = t.lineno
         t = self.lexer.token()
         self.readingString = True
 
-        while not(t.type == "CloseQuote"):
+        while not(t.type == 'RDOUBLEQUOTE'):
             if t.value == '\\':
-                self.errorOutput += '''Error: Se encontró un caracter inesperado "%s" en la Línea %d, Columna %d\n''' % (t.value[0], t.lineno, t.lexpos - self.beginningOfLine)
+                self.errorOutput += '''Error: Se encontró un caracter inesperado "%s" en la Línea %d, Columna %d.\n''' % (t.value[0], t.lineno, t.lexpos - self.beginningOfLine)
                 self.error = True
             if t: 
                 self.output += t.value
                 t = self.lexer.token()
             else: break
-        self.output += quote
-        self.output += " (Línea %d, Columna %d)\n" % (m, n)
+
+            if t.lineno > m:    # Sigue en el ciclo si pasó de linea pero no encontró la comilla de cierre
+                self.errorOutput += '''Error: No se encontró " de cierre del string de la Línea %d, Columna %d.\n''' % (m, n)
+                self.error = True
+                break
+
+        self.output += quote + ") at line %d, column %d\n" % (m, n)
         self.readingString = False
-    
-    t_ANY_ignore_Comment = r'\#.*'
+
+    def t_ANY_error(self, t):
+        if self.readingString:
+            print t.value[0]
+            self.output += t.value[0]
+        else:
+            self.errorOutput += '''Error: Se encontró un caracter inesperado "%s" en la Línea %d, Columna %d\n''' % (t.value[0], t.lineno, t.lexpos - self.beginningOfLine)
+            self.error = True
+        t.lexer.skip(1)
+
+    t_ANY_ignore_COMMENT = r'\#.*'
 
     def __init__(self):
         self.lexer = lex.lex(module=self)
-        
-        
-    
